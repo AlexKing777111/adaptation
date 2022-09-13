eae41a9cda1a427e669d9d1f4a40b8db5bde2810
Alex Korolev <work.alexkorolev@yandex.ru>
2022-09-13 11:23:33 +0500

    change second_threads_task.py

--- second_task/second_threads_task.py
+++ second_task/second_threads_task.py
@@ -1,5 +1,6 @@
 import threading
 import time
+from datetime import datetime
 from random import randint
 
 
@@ -18,31 +19,39 @@ class IntervalThread(threading.Thread):
         super().__init__(group, target, name, args, kwargs, daemon=daemon)
         self.interval = interval
 
-    def run(self):
-        print(f"---Поток {threading.currentThread().name} начал работу.---")
-        time.sleep(randint(1, 10))
-        print(f"---Поток {threading.currentThread().name} закончил работу.---")
+
+def work_imitation():
+    event.clear()
+    print(
+        f"---Поток {threading.currentThread().name} "
+        f"начал работу в {datetime.now()}.---"
+    )
+    time.sleep(randint(1, 10))
+    print(
+        f"---Поток {threading.currentThread().name} "
+        f"закончил работу в {datetime.now()}.---"
+    )
+    thr_reset(
+        name=threading.currentThread().name,
+        interval=threading.currentThread().interval,
+        target=work_imitation,
+    )
+    event.set()
 
 
-def thr1_starter(interval, name):
-    thr1 = IntervalThread(interval=interval, name=name)
+def thr_reset(interval, target, name=None):
+    event.clear()
     time.sleep(interval)
-    thr1.start()
-    return thr1
-
-
-def thr2_starter(interval, name):
-    thr2 = IntervalThread(interval=interval, name=name)
-    time.sleep(interval)
-    thr2.start()
-    return thr2
+    IntervalThread(name=name, interval=interval, target=target).start()
 
 
 if __name__ == "__main__":
-    thr1 = thr1_starter(interval=0, name="Gena")
-    thr2 = thr2_starter(interval=0, name="artur")
-    while True:
-        if not thr1.is_alive():
-            thr1 = thr1_starter(interval=10, name="Gena")
-        elif not thr2.is_alive():
-            thr2 = thr2_starter(interval=1, name="artur")
+    event = threading.Event()
+    event.set()
+    IntervalThread(
+        name="10 second", interval=10, target=work_imitation
+    ).start()
+    IntervalThread(name="7 second", interval=7, target=work_imitation).start()
+    IntervalThread(name="5 second", interval=5, target=work_imitation).start()
+    IntervalThread(name="3 second", interval=3, target=work_imitation).start()
+    IntervalThread(name="1 second", interval=1, target=work_imitation).start()
